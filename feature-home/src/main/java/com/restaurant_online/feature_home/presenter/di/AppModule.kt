851024207486204a09b1b9a16aad4ffc856c6399
Louiixx-h <louix.sm@gmail.com>
2021-12-11 12:11:08 -0800

    comentário removido

--- feature-home/src/main/java/com/restaurant_online/feature_home/presenter/di/AppModule.kt
+++ feature-home/src/main/java/com/restaurant_online/feature_home/presenter/di/AppModule.kt
@@ -1,13 +1,9 @@
 package com.restaurant_online.feature_home.presenter.di
 
-import br.com.restaurant_online.component_domain.RestaurantUseCase
-import br.com.restaurant_online.component_domain.RestaurantUseCaseImpl
 import com.restaurant_online.feature_home.presenter.presenter.viewmodel.HomeViewModel
 import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val presenterModule = module {
-    //single { RestaurantUseCaseImpl(get()) } bind RestaurantUseCase::class
     viewModel { HomeViewModel(get()) }
 }
